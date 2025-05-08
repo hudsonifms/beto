7e84ed07ad11fb689a309fbca672a5f6200e3b73
hudson <hudsonb994@gmail.com>
2025-05-07 20:53:32 -0400

    incial 07/05

--- Beto/exe_ascii.py
+++ Beto/exe_ascii.py
@@ -1,5 +1,6 @@
 ALFABET = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 n = 10
+
 n = int(n/2)
 for l in range(0, n):
     for j in range(1, (n+1)-l):
@@ -12,6 +13,7 @@ for l in range(0, n):
  
     print("")
 for l in range(0, n+1)[::-1]:
+ 
     for j in range(1, (n+1)-l):
         print(" ", end="")
     for j in range(0, l+1):
---
+++ Beto/voagais.py
@@ -0,0 +1,6 @@
+vogais = list("AEIOU".lower())
+frase = "djaklsjdlkad".lower()
+
+for i in frase:
+    if(i in vogais): print(i)
+        
